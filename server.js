490a9ddd43e5cbd2e39e4588f2532ddccb1cab59
Prashant <2024457181.prashant@ug.sharda.ac.in>
2026-04-30 14:44:30 +0530

    Fix catch-all route

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ Be pundit-like, specific, and mention key stats. No bullet points, just flowing 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
   
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
